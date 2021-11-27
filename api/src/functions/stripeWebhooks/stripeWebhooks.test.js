6d2e0604648385cdc689b51b6d5016747ab0d9a7
Dominic Saadi <dominic.saadi@hey.com>
2021-11-26 22:31:54 -0800

    get webhook test passing

--- api/src/functions/stripeWebhooks/stripeWebhooks.test.js
+++ api/src/functions/stripeWebhooks/stripeWebhooks.test.js
@@ -2,25 +2,36 @@ import { mockHttpEvent } from '@redwoodjs/testing/api'
 
 import { handler } from './stripeWebhooks'
 
+import { stripe } from 'src/lib/stripe'
+
 describe('stripeWebhooks function', () => {
   it('Should respond with 200', async () => {
+    /**
+     * Copied from Stripe's tests: {@link https://github.com/stripe/stripe-node/blob/4e82ccafda2017654ac264c070e7ebfa0e662fcd/test/Webhook.spec.js#L8-L12}
+     */
+    const payload = JSON.stringify(
+      {
+        id: 'evt_test_webhook',
+        object: 'event',
+      },
+      null,
+      2
+    )
+
+    const header = stripe.webhooks.generateTestHeaderString({
+      payload,
+      secret: process.env.STRIPE_WEBHOOK_SK,
+    })
+
     const httpEvent = mockHttpEvent({
-      queryStringParameters: {
-        id: '42', // Add parameters here
+      body: payload,
+      headers: {
+        'stripe-signature': header,
       },
     })
 
     const response = await handler(httpEvent, null)
-    const { data } = JSON.parse(response.body)
 
     expect(response.statusCode).toBe(200)
-    expect(data).toBe('stripeWebhooks function')
   })
-
-  // You can also use scenarios to test your api functions
-  // See guide here: https://redwoodjs.com/docs/testing#scenarios
-  //
-  // scenario('Scenario test', async () => {
-  //
-  // })
 })
