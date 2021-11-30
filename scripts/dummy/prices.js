63b2afddfbe68514f0aaa6c126004bcfb065a886
chris <chrsvdmrw@gmail.com>
2021-11-30 16:15:06 +0200

    create products and prices

---
+++ scripts/dummy/prices.js
@@ -0,0 +1,18 @@
+export const dummyPrices = [
+  {
+    currency: 'usd',
+    unit_amount: 2000,
+    product_data: {
+      id: 'prod_sp10X1Y2Z3001xvcx',
+      name: 'Invisibility',
+    },
+  },
+  {
+    currency: 'usd',
+    unit_amount: 3500,
+    product_data: {
+      id: 'prod_sp20Z3Y2X1002xvcx',
+      name: 'Flight',
+    },
+  },
+]
--- scripts/seed_products.js
+++ scripts/seed_products.js
@@ -1,8 +1,20 @@
 // To access your database
 import { db } from '$api/src/lib/db'
+import { dummyPrices } from './dummy/prices'
+
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SK)
 
 export default async ({ args }) => {
-  // Your script here...
+  const priceResults = []
+  for (const price of dummyPrices) {
+    const result = await stripe.prices.create(price, {
+      idempotencyKey: `${price.product_data.id}ik`,
+    })
+    priceResults.push(result)
+  }
+
+  console.log(priceResults)
   console.log(':: Executing script with args ::')
   console.log(args)
 }
