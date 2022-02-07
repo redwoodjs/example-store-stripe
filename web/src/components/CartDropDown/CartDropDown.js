13149a6b09a5fb636701dce88fa2c7e9a3404518
chris <chrsvdmrw@gmail.com>
2022-02-07 19:36:00 +0200

    checkout p2

--- web/src/components/CartDropDown/CartDropDown.js
+++ web/src/components/CartDropDown/CartDropDown.js
@@ -1,10 +1,17 @@
 import { List } from '../List/List'
 import { CartDropDownItem } from 'src/components/CartDropDownItem/CartDropDownItem'
 
+import { useMutation } from '@redwoodjs/web'
+
+const MUTATION = gql``
+
 export const CartDropDown = () => {
+  const [mutate] = useMutation(MUTATION)
   const cartItems = [{ item: 'Invisibility' }, { item: 'Flight' }]
 
-  const onCheckoutButtonClick = () => {}
+  const onCheckoutButtonClick = () => {
+    mutate({})
+  }
 
   return (
     <div className="cart-drop-down">
