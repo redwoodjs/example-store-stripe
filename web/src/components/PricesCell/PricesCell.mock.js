50de923b91cfe54ba9fca83ca06c7d4082108413
chris <chrsvdmrw@gmail.com>
2022-02-09 21:20:46 +0200

    Fix mock file

--- web/src/components/PricesCell/PricesCell.mock.js
+++ web/src/components/PricesCell/PricesCell.mock.js
@@ -1,4 +1,21 @@
 // Define your own mock data here:
 export const standard = () => ({
-  prices: [{ id: 42 }, { id: 43 }, { id: 44 }],
+  prices: [
+    {
+      product: {
+        images: [
+          'https://files.stripe.com/links/MDB8YWNjdF8xSzFVWTF…3Rlc3RfanFNckFLN1dJaEt3VW9wcGxSbnZVVFJq00iViXZF5l',
+        ],
+        name: 'Flight',
+      },
+    },
+    {
+      product: {
+        images: [
+          'https://files.stripe.com/links/MDB8YWNjdF8xSzFVWTF…3Rlc3RfanFNckFLN1dJaEt3VW9wcGxSbnZVVFJq00iViXZF5l',
+        ],
+        name: 'Invisibility',
+      },
+    },
+  ],
 })
