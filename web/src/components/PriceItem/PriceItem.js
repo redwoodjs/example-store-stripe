efd1c766cb851d33de932c8d32cd6a2a2c37f79b
chris <chrsvdmrw@gmail.com>
2022-02-09 20:51:06 +0200

    Fix linting errors

--- web/src/components/PriceItem/PriceItem.js
+++ web/src/components/PriceItem/PriceItem.js
@@ -1,26 +1,7 @@
-import { useMutation } from '@redwoodjs/web'
-
-// Temporarily commented out until cart feat
-// const MUTATION = gql`
-// mutation AddToStripeCart($priceId: String) {
-//   addToStripeCart(priceId: $priceId) {
-//     items
-//   }
-// }`
-
-export const PriceItem = ({
-  id,
-  nickname,
-  product: { images, name, description },
-}) => {
-  // const [mutate] = useMutation(MUTATION)
-
-  const onCartButtonClick = () => {
-    // mutate({priceId: id})
-    // handleAddToCart(id)
-    // const cart = localStorage.getItem('items')
-    // localStorage.setItem('items', ['dfdfgdfg', 'dfdfsfs'])
-  }
+export const PriceItem = ({ product: { images, name } }) => {
+  // const onCartButtonClick = () => {
+  //   // ignore
+  // }
 
   return (
     <div className="price-item--card">
@@ -28,7 +9,7 @@ export const PriceItem = ({
         <img alt={`A human using the ${name} superpower`} src={images[0]} />
         <figcaption>{name}</figcaption>
       </figure>
-      <button onClick={onCartButtonClick}>Add to Cart</button>
+      {/* <button onClick={onCartButtonClick}>Add to Cart</button> */}
     </div>
   )
 }
