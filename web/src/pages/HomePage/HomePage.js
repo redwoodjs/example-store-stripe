a7ad10eda675e46b7c1054d694af53b96c62a286
Dominic Saadi <dominic.saadi@hey.com>
2022-02-17 14:04:51 +0900

    style: typos

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,14 +3,14 @@ import PricesCell from 'src/components/PricesCell'
 const HomePage = () => {
   return (
     <>
-      <h2>Super tokens</h2>
+      <h2>Super Tokens</h2>
       <p>These are single-use tokens. Great for emergencies and gifts.</p>
       <PricesCell priceType="one_time" />
 
       <h2>Super Subs</h2>
       <p>
-        Monthly subscriptions to superpowers. Great for the carreer
-        supervillian, hero or parent.
+        Monthly subscriptions to superpowers. Great for the career supervillain,
+        hero or parent.
       </p>
       <PricesCell priceType="recurring" />
     </>
