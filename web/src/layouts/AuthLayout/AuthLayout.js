f4a339e371b3eb514ff9ccf88ed6781080346ad5
chris <chrsvdmrw@gmail.com>
2022-02-28 09:12:54 +0200

    Add AuthLayout

---
+++ web/src/layouts/AuthLayout/AuthLayout.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+import Footer from 'src/components/Footer'
+
+const AuthLayout = ({ children }) => {
+  return (
+    <Grid>
+      <Row>
+        <Gradient>SuperPOW!!!</Gradient>
+      </Row>
+      <Column>{children}</Column>
+      <Footer />
+    </Grid>
+  )
+}
+
+export default AuthLayout
+
+// Styles
+
+const Grid = styled.div`
+  --breathing-room: var(--size-3);
+
+  display: grid;
+  grid-template-columns:
+    1fr
+    min(75ch, 100%)
+    1fr;
+  padding-left: var(--breathing-room);
+  padding-right: var(--breathing-room);
+  row-gap: var(--breathing-room);
+
+  & > * {
+    grid-column: 2;
+  }
+
+  & > *:first-child {
+    padding-top: var(--breathing-room);
+  }
+`
+
+const Row = styled.header`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Gradient = styled.h1`
+  background: var(--gradient-3);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const Column = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  gap: var(--size-3);
+`
---
+++ web/src/layouts/AuthLayout/AuthLayout.stories.js
@@ -0,0 +1,7 @@
+import AuthLayout from './AuthLayout'
+
+export const generated = () => {
+  return <AuthLayout />
+}
+
+export default { title: 'Layouts/AuthLayout' }
