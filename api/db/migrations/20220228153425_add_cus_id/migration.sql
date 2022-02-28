ce7d8cdd08bc9657799ce6a44b67420d4006bc1b
chris <chrsvdmrw@gmail.com>
2022-02-28 22:22:06 +0200

    Fix migration issue

--- api/db/migrations/20220228153425_add_cus_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `customerId` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "customerId" TEXT NOT NULL DEFAULT cus00001;
