e92bdf19b01da8e6891c8814ec49077118625de8
izhar360 <muhammadizhar360@gmail.com>
2022-03-05 12:20:43 +0500

    disabled lint warnings

--- web/src/components/Product/Product.js
+++ web/src/components/Product/Product.js
@@ -12,7 +12,12 @@ const Product = ({ id, name, description, price, image, type }) => {
         <Image alt={description} src={image} />
         <Info>
           <Name>{name}</Name>
-          <Price>{(price/100).toLocaleString("en-US", {style:"currency", currency:"USD"})}</Price>
+          <Price>
+            {(price / 100).toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })}
+          </Price>
         </Info>
       </Wrapper>
     </article>
