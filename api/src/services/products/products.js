5a8ea9727e29d607aafb674186c69c45a4296a0f
chris <chrsvdmrw@gmail.com>
2022-03-08 12:29:59 +0200

    filter prices for active products

--- api/src/services/products/products.js
+++ api/src/services/products/products.js
@@ -6,18 +6,20 @@ export const products = async ({ type = 'one_time' }) => {
     expand: ['data.product'],
   })
 
-  const products = prices.data.map((price) => {
-    const { product } = price
+  const products = prices.data
+    .filter(({ product }) => product.active)
+    .map((price) => {
+      const { product } = price
 
-    return {
-      id: price.id,
-      name: product.name,
-      description: product.description,
-      image: product.images[0],
-      price: price.unit_amount,
-      type: price.type,
-    }
-  })
+      return {
+        id: price.id,
+        name: product.name,
+        description: product.description,
+        image: product.images[0],
+        price: price.unit_amount,
+        type: price.type,
+      }
+    })
 
   return products
 }
