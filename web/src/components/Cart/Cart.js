3ca27dfcba4301ce0b2e8da49696ac2075ee9c88
chris <chrsvdmrw@gmail.com>
2022-03-23 15:45:25 +0200

    Add indicator to cart

--- web/src/components/Cart/Cart.js
+++ web/src/components/Cart/Cart.js
@@ -1,19 +1,24 @@
 import { useState } from 'react'
+import styled, { css } from 'styled-components'
 
 import CartDropDown from 'src/components/CartDropDown'
 import Button from 'src/components/Button'
 
+import { useCart } from 'src/components/CartProvider'
+
 const Cart = () => {
   const [isVisible, setVisibility] = useState(false)
+  const cartLength = useCart().length
 
   const toggleVisibility = () => setVisibility(!isVisible)
 
   return (
     <>
-      <Button
+      <IndicatorButton
         onClick={toggleVisibility}
         icon="shoppingCart"
         active={isVisible}
+        data-items={cartLength}
       />
       {isVisible && <CartDropDown toggleVisibility={toggleVisibility} />}
     </>
@@ -21,3 +26,26 @@ const Cart = () => {
 }
 
 export default Cart
+
+/* Styles */
+
+const IndicatorButton = styled(Button)`
+  position: relative;
+
+  svg {
+    stroke: var(--primary);
+  }
+
+    &:after {
+      content: attr(data-items);
+      display: block;
+      position: absolute;
+      top: -0.1em;
+      right: -0.1em;
+      padding: 0 0.4em;
+      border-radius: 1em;
+      font-size: var(--font-size-0);
+      background: var(--primary);
+      color: var(--white);
+
+`
