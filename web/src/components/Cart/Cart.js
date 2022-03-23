36f027d7ea016d58477850c1a1bdb78254c88478
chris <chrsvdmrw@gmail.com>
2022-03-23 16:37:16 +0200

    Enhance/add item indicator to cart (#132)
    * Add indicator to cart
    
    * Fix cart quantity

--- web/src/components/Cart/Cart.js
+++ web/src/components/Cart/Cart.js
@@ -1,19 +1,35 @@
 import { useState } from 'react'
+import styled, { css } from 'styled-components'
 
 import CartDropDown from 'src/components/CartDropDown'
 import Button from 'src/components/Button'
 
+import { useCart } from 'src/components/CartProvider'
+
 const Cart = () => {
   const [isVisible, setVisibility] = useState(false)
+  const cart = useCart()
 
   const toggleVisibility = () => setVisibility(!isVisible)
+  const getCartQuantity = () => {
+    if (cart.length > 0) {
+      const length = cart.reduce((total, item) => {
+        return total + item.quantity
+      }, 0)
+      return length
+    }
+    return 0
+  }
+
+  const quantity = getCartQuantity()
 
   return (
     <>
-      <Button
+      <IndicatorButton
         onClick={toggleVisibility}
         icon="shoppingCart"
         active={isVisible}
+        data-quantity={quantity}
       />
       {isVisible && <CartDropDown toggleVisibility={toggleVisibility} />}
     </>
@@ -21,3 +37,26 @@ const Cart = () => {
 }
 
 export default Cart
+
+/* Styles */
+
+const IndicatorButton = styled(Button)`
+  position: relative;
+
+  svg {
+    stroke: var(--primary);
+  }
+
+    &:after {
+      content: attr(data-quantity);
+      display: block;
+      position: absolute;
+      top: -0.1em;
+      right: -0.1em;
+      padding: 0 0.4em;
+      border-radius: 1em;
+      font-size: var(--font-size-0);
+      background: var(--primary);
+      color: var(--white);
+
+`
