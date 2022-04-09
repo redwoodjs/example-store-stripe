1fb117b9415aa0e8c85ae4465e73f64586f9bd87
Dominic Saadi <dominic.saadi@hey.com>
2022-04-09 19:58:59 +0900

    Document AuthLayout

--- web/src/layouts/AuthLayout/AuthLayout.js
+++ web/src/layouts/AuthLayout/AuthLayout.js
@@ -1,9 +1,14 @@
+// This handles a user coming straight to /login.
+// Unlikely, but still.
 import { Redirect, routes } from '@redwoodjs/router'
 import { useAuth } from '@redwoodjs/auth'
 
 const AuthLayout = ({ children }) => {
   const { loading, isAuthenticated } = useAuth()
 
+  // If auth is loading, it's really important we return null.
+  // Otherwise we flash users with content, then redirect them
+  // (if it turns out they're authenticated after all)
   if (loading) {
     return null
   }
@@ -12,7 +17,7 @@ const AuthLayout = ({ children }) => {
     return <Redirect to={routes.home()} />
   }
 
-  return <>{children}</>
+  return children
 }
 
 export default AuthLayout
