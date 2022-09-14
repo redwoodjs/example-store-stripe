7700c8da8f706db981193d3b694ab20963456f01
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-09-14 17:35:22 +0700

    lint fix

--- web/src/pages/ResetPasswordPage/ResetPasswordPage.js
+++ web/src/pages/ResetPasswordPage/ResetPasswordPage.js
@@ -1,12 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 
 import { useAuth } from '@redwoodjs/auth'
-import {
-  Form,
-  Label,
-  PasswordField,
-  FieldError,
-} from '@redwoodjs/forms'
+import { Form, Label, PasswordField, FieldError } from '@redwoodjs/forms'
 import { navigate, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
