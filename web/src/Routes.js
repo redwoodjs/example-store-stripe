547e94ec83954e22b14b4d7516a98974e70867e5
chris <chrsvdmrw@gmail.com>
2023-01-17 14:28:55 +0200

    add StripeProvider to Routes.js

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,6 +7,9 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
+import { StripeProvider } from 'redwoodjs-stripe/web'
+
+import { useAuth } from '@redwoodjs/auth'
 import { Router, Route, Set } from '@redwoodjs/router'
 
 import AuthRedirect from 'src/components/AuthRedirect'
@@ -14,10 +17,29 @@ import CartProvider from 'src/components/CartProvider'
 import MainLayout from 'src/layouts/MainLayout'
 
 const Routes = () => {
+  /*
+    Passing the authenticated user to StripeProvider for use for Stripe Checkout and Stripe Portals
+    User needs to manage authentication of customers
+  */
+  const { currentUser, isAuthenticated } = useAuth()
+  let authUser = currentUser || {
+    email: null,
+    name: null,
+  }
+
   return (
     <Router>
-      <Route path="/stripe-demo" page={StripeDemoPage} name="stripeDemo" />
-      <Set wrap={[CartProvider, MainLayout]}>
+      {/* <Route path="/stripe-demo" page={StripeDemoPage} name="stripeDemo" /> */}
+      <Set
+        wrap={[CartProvider, MainLayout, StripeProvider]}
+        customer={{
+          search: isAuthenticated ? `email: "${authUser.email}"` : '',
+          create: {
+            email: authUser.email,
+            name: authUser.name,
+          },
+        }}
+      >
         <Route path="/" page={HomePage} name="home" />
         <Route path="/success" page={SuccessPage} name="success" />
         <Route path="/failure" page={FailurePage} name="failure" />
