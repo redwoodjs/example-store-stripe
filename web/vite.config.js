34b0d7c41e4b9cd1108c0e38970744d9f688f015
chris <chrsvdmrw@gmail.com>
2024-03-12 13:47:38 +0200

    upgrade rw v6

---
+++ web/vite.config.js
@@ -0,0 +1,19 @@
+import dns from 'dns'
+
+import { defineConfig } from 'vite'
+
+import redwood from '@redwoodjs/vite'
+
+// See: https://vitejs.dev/config/server-options.html#server-host
+// So that Vite will load on local instead of 127.0.0.1
+dns.setDefaultResultOrder('verbatim')
+
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
+const viteConfig = {
+  plugins: [redwood()],
+}
+
+export default defineConfig(viteConfig)
