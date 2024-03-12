55d5d858930098cc62a71e6b053acf908be4e2b3
chris <chrsvdmrw@gmail.com>
2024-03-12 14:36:16 +0200

    lint fixes

--- web/src/components/Spinner/SpinnerSVG.jsx
+++ web/src/components/Spinner/SpinnerSVG.jsx
@@ -1,12 +1,20 @@
 const SpinnerSVG = () => {
-  return (<svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#000">
-    <g fill="none" fillRule="evenodd">
+  return (
+    <svg
+      width="38"
+      height="38"
+      viewBox="0 0 38 38"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#000"
+    >
+      <g fill="none" fillRule="evenodd">
         <g transform="translate(1 1)" strokeWidth="2">
-            <circle strokeOpacity=".25" cx="18" cy="18" r="18"/>
-            <path d="M36 18c0-9.94-8.06-18-18-18" />
+          <circle strokeOpacity=".25" cx="18" cy="18" r="18" />
+          <path d="M36 18c0-9.94-8.06-18-18-18" />
         </g>
-    </g>
-</svg>)
+      </g>
+    </svg>
+  )
 }
 
 export default SpinnerSVG
