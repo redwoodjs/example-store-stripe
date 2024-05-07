fbeec3e032a953ce0552e8158d6500823c2f9e97
chris <chrsvdmrw@gmail.com>
2024-05-07 13:34:34 +0200

    Update counter to use custom attribute

--- web/src/components/RWSCart/RWSCart.jsx
+++ web/src/components/RWSCart/RWSCart.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react'
+
+import { useStripeCart } from '@redwoodjs-stripe/web'
+
 import styles from './styles.module.css'
 
 const RwsCart = () => {
+  const [isCartVisible, setCartVisibilty] = useState(false)
+  const { cart } = useStripeCart()
+  const count = cart.reduce((prev, cur) => prev + cur.quantity, 0)
+  const visCount = count > 0 ? count : null
+
+  const onCartButtonClick = () => {
+    setCartVisibilty(!isCartVisible)
+  }
+
   return (
-    <>
-      <button className={styles.rwsButton}>Cart</button>
-    </>
+    <button
+      className={styles.rwsButton}
+      onClick={onCartButtonClick}
+      data-active={isCartVisible}
+      data-counter={visCount}
+    >
+      Cart
+    </button>
   )
 }
+
+// const CartCounter = () => {
+//   const { cart } = useStripeCart()
+//   const count = cart.reduce((prev, cur) => prev + cur.quantity, 0)
+//   return count > 0 ? <span className={styles.rwsCounter}>{count}</span> : null
+// }
 
 export default RwsCart
